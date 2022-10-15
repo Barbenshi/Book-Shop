4a196120e21c9f8473fe2fff9afa676998e1e67d
Bar Ben Shimol <barbenshimol2@gmail.com>
2022-10-15 14:22:23 +0300

    YESS

--- js/books-controller.js
+++ js/books-controller.js
@@ -36,8 +36,8 @@ function renderBooks() {
         strHtmls = books.map(book =>
             `
             <div class="book">
-                <div class="description"><span data-trans="${book.name.toLowerCase()}">${book.name}</span>, ${formatCurrency(book.price)}</div>
-                <img src="./img/book-pictures/${book.image}.jfif" alt="book img"/>
+                <div class="description text-warning"><span data-trans="${book.name.toLowerCase()}">${book.name}</span>, ${formatCurrency(book.price)}</div>
+                <img src="./img/book-pictures/${book.image}.jpg" alt="book img"/>
                 <div class="book-actions"><button class="read btn btn-primary" onclick="onReadBook('${book.id}')" data-trans="read">Read</button>
                 <button class="update btn btn-warning" onclick="onUpdateBook('${book.id}')" data-trans="update">Update</button>
                 <button class="remove btn btn-danger" onclick="onRemoveBook('${book.id}')">X</button> </div>
